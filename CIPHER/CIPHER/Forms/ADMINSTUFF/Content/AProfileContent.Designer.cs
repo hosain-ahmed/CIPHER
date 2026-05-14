e0df802c4720e987b0ebd1e5cf15da56813b1f15
Hosain <hosainahmedwriter@gmail.com>
2026-05-15 01:43:54 +0600

    checking

--- CIPHER/CIPHER/Forms/ADMINSTUFF/Content/AProfileContent.Designer.cs
+++ CIPHER/CIPHER/Forms/ADMINSTUFF/Content/AProfileContent.Designer.cs
@@ -28,8 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            SuspendLayout();
+            // 
+            // AProfileContent
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Name = "AProfileContent";
+            Size = new Size(1244, 779);
+            ResumeLayout(false);
         }
 
         #endregion
